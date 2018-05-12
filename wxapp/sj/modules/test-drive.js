ae708c753cb0be63ef3193df6d5f00d46aa7e2ee
hx-dl <546506879@qq.com>
2018-05-12 16:32:57 +0800

    小程序学习

---
+++ wxapp/sj/modules/test-drive.js
@@ -0,0 +1,10 @@
+// es6 模块化语法  module
+// js  变量或常量，它的类型由值决定
+const testDrive = () => {
+    // 用内置的来自微信的API
+    // 原生的API   大部分的是HTML5 webview  性能上比原生的慢一些    webview nativeview
+  wx.showToast({
+      title:'暂不支持'
+  })
+}
+export default testDrive
