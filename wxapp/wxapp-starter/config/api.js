186c3702b119bb9145d4db386923e97c00b07ade
hx-dl <546506879@qq.com>
2018-05-18 11:27:14 +0800

    移除了小程序相关文件

--- wxapp/wxapp-starter/config/api.js
+++
@@ -1,2 +0,0 @@
-export const API_BASE = "https://www.easy-mock.com/mock/5afc29fee5c64d22cc1ca4a0"
-export const API_CREATE = "https://www.easy-mock.com/mock/5af03ac5957fd4308a30b9c0/api/create#!method=get"
